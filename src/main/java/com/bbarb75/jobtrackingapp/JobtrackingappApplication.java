bd39fa75e7f47f4884f6987ce648cffdcece526c
Brian Barbour <bkbarbour@gmail.com>
2024-08-12 21:13:53 -0400

    Initial commit - setting up project

---
+++ src/main/java/com/bbarb75/jobtrackingapp/JobtrackingappApplication.java
@@ -0,0 +1,13 @@
+package com.bbarb75.jobtrackingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtrackingappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobtrackingappApplication.class, args);
+    }
+
+}
