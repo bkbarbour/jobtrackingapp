b429bbc7e52b036311f061acba39680b80e73f62
Brian Barbour <bkbarbour@gmail.com>
2024-08-15 20:09:46 -0400

    Further work on controllers - adding save function

--- src/main/java/com/bbarb75/jobtrackingapp/controllers/AddJobController.java
+++ src/main/java/com/bbarb75/jobtrackingapp/controllers/AddJobController.java
@@ -5,9 +5,11 @@ import com.bbarb75.jobtrackingapp.service.JobAppService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AddJobController {
@@ -19,9 +21,21 @@ public class AddJobController {
     }
 
     @GetMapping("/addjob")
-    public String addJob(@Validated @ModelAttribute("jobApp") JobApp jobApp, Model model) {
+    public String addJob(Model model) {
+        JobApp jobApp = new JobApp();
         model.addAttribute("jobApp", jobApp);
         return "addjob";
+    }
+
+    @PostMapping("/addjob")
+    public String submitJob(@Validated @ModelAttribute("jobApp") JobApp jobApp, BindingResult bindingResult, Model model) {
+        model.addAttribute("jobApp", jobApp);
+        if (bindingResult.hasErrors()) {
+            return "addjob";
+        }else{
+            jobAppService.save(jobApp);
+            return "mainscreen";
+        }
     }
 
 
